fd73162b3d631c6de8729544c924b38c209256ba
Seymour James <seymourpjames@gmail.com>
2022-01-30 00:09:20 -0700

    changes to server

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const app = express();
 
-const port = 3001;
-
 app.use(bodyParser.json({ type: "application/*+json" }));
 app.use(cors());
+app.use(dotenv());
 
 app.get("/coins", async (req, res) => {
   const { data } = await axios.get("https://api.coinranking.com/v2/coins", {
     headers: {
       "x-access-token":
-        "coinrankingae7bc3454a696a664b91a561b298da166cbc197a0de7683e",
+        process.env.TOKEN,
     },
   });
   res.json(data);
 });
 
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`));
