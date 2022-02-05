3e40992988aceb0f05a49f8deb408d117f5ea38b
Seymour James <seymourpjames@gmail.com>
2022-02-04 23:45:45 -0700

    url change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json({ type: "application/*+json" }));
 app.use(cors());
 
-app.get("/coins", async (req, res) => {
+app.get("/", async (req, res) => {
   const { data } = await axios.get("https://api.coinranking.com/v2/coins", {
     headers: {
       "x-access-token":
