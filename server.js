4b7e3e449662fcf1158427827e6564e5aba5aa84
Seymour James <seymourpjames@gmail.com>
2022-02-17 20:09:30 -0700

    .

--- server.js
+++ server.js
@@ -9,13 +9,16 @@ app.use(bodyParser.json({ type: "application/*+json" }));
 app.use(cors());
 
 app.get("/", async (req, res) => {
-  const { data } = await axios.get("https://api.coinranking.com/v2/coins", {
-    headers: {
-      "x-access-token":
-        process.env.TOKEN,
-    },
-  });
-  res.json(data);
+  try {
+    const { data } = await axios.get("https://api.coinranking.com/v2/coins", {
+      headers: {
+        "x-access-token": process.env.TOKEN,
+      },
+    });
+    res.json(data);
+  } catch (error) {
+    res.send(error.message).statusCode(503);
+  }
 });
 
 app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`));
